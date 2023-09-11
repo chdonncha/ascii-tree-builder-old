ade567915ab161e78e73da1af93fca66e1392db1
donncha <chdonncha@gmail.com>
2023-09-11 23:21:31 +0100

    Init textboxes

--- ascii-tree-builder/src/App.js
+++ ascii-tree-builder/src/App.js
@@ -5,18 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <div style={{ display: 'flex' }}>
+              <input type="text" placeholder="Enter some text" className="main-text-box" style={{ marginRight: '50px' }}  />
+              <input type="text" placeholder="Enter some text" className="main-text-box" />
+          </div>
       </header>
     </div>
   );
