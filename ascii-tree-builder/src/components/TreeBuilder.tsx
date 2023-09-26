59e77811a0930adf95ba9c1f91ef2409348bfdc6
chdonncha <chdonncha@gmail.com>
2023-09-27 00:43:02 +0100

    ♻️ Remove commented code

--- ascii-tree-builder/src/components/TreeBuilder.tsx
+++ ascii-tree-builder/src/components/TreeBuilder.tsx
@@ -60,54 +60,10 @@ const TreeBuilder: React.FC = () => {
     };
   }, []);
 
-  // const deleteRow = () => {
-  //   if (selectedRow === -1) return; // If no row is selected, do nothing
-  //
-  //   const newRows = [...rows];
-  //
-  //   // Identify the children to delete based on indentation
-  //   const currentIndentation = getIndentation(newRows[selectedRow].content);
-  //
-  //   let lastIndexToDelete = selectedRow;
-  //   for (let i = selectedRow + 1; i < newRows.length; i++) {
-  //     if (getIndentation(newRows[i].content) > currentIndentation) {
-  //       lastIndexToDelete = i;
-  //     } else {
-  //       break;
-  //     }
-  //   }
-  //
-  //   // Delete the selected row and its children
-  //   newRows.splice(selectedRow, lastIndexToDelete - selectedRow + 1);
-  //
-  //   // Adjust the selected row after deletion
-  //   let newRowToSelect = -1;
-  //
-  //   // If it wasn't the first row, select the previous row
-  //   if (selectedRow > 0) {
-  //     newRowToSelect = selectedRow - 1;
-  //   }
-  //   // If it was the first row and there are still rows available, select the next row
-  //   else if (selectedRow === 0 && newRows.length > 0) {
-  //     newRowToSelect = 0;
-  //   }
-  //
-  //   setRows(newRows);
-  //   setSelectedRow(newRowToSelect);
-  // };
-
   const getIndentation = (str: string): number => {
     const match = str.match(/^ */);
     return match ? match[0].length : 0;
   };
-
-  // const startRenaming = () => {
-  //   if (selectedRow >= 0) {
-  //     addToUndoStack(rows);
-  //     setRenameValue(rows[selectedRow].content.trim());
-  //     setIsRenaming(true);
-  //   }
-  // };
 
   const handleRenameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setRenameValue(e.target.value);
