0dc42db773ddd4f9d125ac30bfeec19ac6e6999d
chdonncha <chdonncha@gmail.com>
2023-10-02 00:07:45 +0100

    💬 Add Instructions

--- ascii-tree-builder/src/App.tsx
+++ ascii-tree-builder/src/App.tsx
@@ -6,11 +6,36 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-          <h1>ASCII Tree Builder</h1>
-          <TreeBuilder />
-        </div>
+        <h3>ASCII Tree Builder</h3>
       </header>
+      <div className="instructions-container">
+        <strong>Tree Builder Instructions:</strong>
+        <ul>
+          <li>
+            <strong>Manipulating Structure:</strong> Use the arrow buttons (↑, ↓, ←, →) to adjust the position and
+            indentation of your selected tree item.
+          </li>
+          <li>
+            <strong>Editing:</strong> Click 'Delete' to remove an item and its children, 'Rename' to modify its name, or
+            'Clear' to reset the entire tree.
+          </li>
+          <li>
+            <strong>Undo/Redo:</strong> Made a mistake? Utilise the 'Undo' and 'Redo' buttons to navigate your recent
+            changes.
+          </li>
+          <li>
+            <strong>ASCII Format:</strong> Once satisfied, view and copy your tree in ASCII format for documentation or
+            future reference.
+          </li>
+          <li>
+            <strong>Importing:</strong> To continue working on a structure, paste the copied ASCII text into the Import
+            Tree section.
+          </li>
+        </ul>
+      </div>
+      <div className="tree-container">
+        <TreeBuilder />
+      </div>
     </div>
   );
 }
