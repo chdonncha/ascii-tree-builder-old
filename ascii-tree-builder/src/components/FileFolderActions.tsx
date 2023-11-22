3189688e9bd02eb149cd415b569225a413ca33de
chdonncha <chdonncha@gmail.com>
2023-11-22 18:26:10 +0000

    🐛 Fix make new added file or folder to be added directly below the selected children

--- ascii-tree-builder/src/components/FileFolderActions.tsx
+++ ascii-tree-builder/src/components/FileFolderActions.tsx
@@ -8,73 +8,56 @@ interface FileFolderActionsProps {
   selectedRow: number;
   addToUndoStack: (rows: any) => void;
 }
+
 export const FileFolderActions: React.FC<FileFolderActionsProps> = ({ rows, setRows, selectedRow, addToUndoStack }) => {
-  const addFile = () => {
-    addToUndoStack(rows);
-    const newFileName = prompt('Enter the new file name:');
-    if (newFileName) {
-      const newRows = [...rows];
-      let newId = generateUniqueId();
-      let parentId = null;
-      let level = 0;
-
-      if (selectedRow >= 0) {
-        parentId = rows[selectedRow].id;
-        level = rows[selectedRow].level;
-      }
-
-      // Insert the new file
-      newRows.push({
-        id: newId,
-        parentId: parentId,
-        level: level + 1, // increment level by one if it's a child
-        content: newFileName,
-        isSelected: false,
-        type: 'file',
-      });
-
-      setRows(newRows);
-    }
-  };
-
-  const addFolder = () => {
-    addToUndoStack(rows);
-    let newFolderName = prompt('Enter the new folder name:');
-    if (newFolderName) {
-      newFolderName = newFolderName.trim().replace(/\/+$/, '') + '/';
-      const newRows = [...rows];
-      let newId = generateUniqueId();
-      let parentId = null;
-      let level = 0;
-
-      if (selectedRow >= 0) {
-        parentId = rows[selectedRow].id;
-        level = rows[selectedRow].level;
-      }
-
-      newRows.push({
-        id: newId,
-        parentId: parentId,
-        level: level,
-        content: newFolderName,
-        isSelected: false,
-        type: 'folder',
-      });
-
-      setRows(newRows);
-    }
-  };
-
   function generateUniqueId() {
     return '_' + Math.random().toString(36).substr(2, 9);
   }
 
+  const addItem = (type: 'file' | 'folder') => {
+    addToUndoStack(rows);
+    let newItemName = prompt(`Enter the new ${type} name:`);
+    if (newItemName) {
+      if (type === 'folder') {
+        newItemName = newItemName.trim().replace(/\/+$/, '') + '/';
+      }
+      let newRows = [...rows];
+      let newId = generateUniqueId();
+      let parentId = null;
+      let level = 0;
+      let insertionIndex = selectedRow + 1; // Default to insert after the selected row
+
+      if (selectedRow >= 0) {
+        parentId = rows[selectedRow].id;
+        level = rows[selectedRow].level + 1; // Increment to make it a child
+        // Find the correct insertion index
+        for (let i = selectedRow + 1; i < rows.length; i++) {
+          if (rows[i].level <= rows[selectedRow].level) {
+            break;
+          }
+          insertionIndex++;
+        }
+      }
+
+      newRows.splice(insertionIndex, 0, {
+        id: newId,
+        parentId: parentId,
+        level: level,
+        content: newItemName,
+        isSelected: false,
+        type: type,
+      });
+
+      setRows(newRows);
+    }
+  };
+
   return (
     <>
-      <Button variant="contained" className="button-style" onClick={addFolder}>
+      <Button variant="contained" className="button-style" onClick={() => addItem('folder')}>
         Add Folder
       </Button>
-      <Button variant="contained" className="button-style" onClick={addFile}>
+      <Button variant="contained" className="button-style" onClick={() => addItem('file')}>
         Add File
       </Button>
     </>
